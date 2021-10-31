948f6f1c0d0344eee3d7ea3643c23c8ee4722bb9
mustakrakishe <mustakrkish@gmail.com>
2021-10-31 17:57:24 +0300

    Installed elasticsearch-php. Created index view.

---
+++ index.php
@@ -0,0 +1,37 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <script src="js/jquery-3.6.0.min.js"></script>
+</head>
+
+<body>
+    <div class="container px-0 py-4">
+
+        <a href="/" class="text-dark text-decoration-none">
+            <h1 class="text-center mb-3">Users</h1>
+        </a>
+
+        <table id="users-table" class="table table-striped text-center">
+            <thead>
+                <tr>
+                    <th scope="col">Age</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">E-mail</th>
+                    <th scope="col">Phone</th>
+                </tr>
+            </thead>
+
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
